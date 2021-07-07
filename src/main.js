1fb6e6c74160394f5da2210f27ab1c3e79e1b15f
Gina <floresgina2020@gmail.com>
2021-07-07 15:19:48 -0500

    probando funciones en el main y aplicando responsive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
+
 const containerAnimes = document.getElementById('container-animes');
 
 data.films.forEach((dato) => {
@@ -9,11 +10,34 @@ data.films.forEach((dato) => {
     cardAnime.innerHTML = `
     <div>
     <img src= '${dato.poster}' id="image-poster"></img>
-    <p id="title"> ${dato.title} </p>
+    <p id="title"> ${dato.title}
+    <a href="title.html"> ${dato.title}</a>
+    </p>
     <p id="release"> ${"(" + dato.release_date + ")"} </p>
     <br>
     </div>`
     containerAnimes.appendChild(cardAnime);
 });
+
+
+
+
+console.log(data.films.map(director => director.director));
+
+
+
+document.addEventListener("click", function(){
+    document.getElementById("root").innerHTML = JSON.stringify(dire);
+  });
+
+
+const directores = document.getElementById("direct1");
+const dire= data.films.filter(item => 
+   item.director.includes(directores.value));
+
+console.log(dire);
+
+console.log(data.films.filter(item => item.director.includes(directores.value)));
+
 
 //console.log(example, data);
