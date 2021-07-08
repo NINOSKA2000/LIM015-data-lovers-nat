6b075c95b92b0796d2d6399b4d6505dde7b81452
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2021-07-08 09:09:46 -0500

    cambios en filtros

--- src/main.js
+++ src/main.js
@@ -1,55 +1,14 @@
-//import { example } from './data.js';
-import data from './data/ghibli/ghibli.js';
-
-
-const containerAnimes = document.getElementById('container-animes');
-
-
-
-
-data.films.forEach(datos);
-
-function datos(dato) {
-    const cardAnime = document.createElement('div')
-    cardAnime.className = "container-card-anime";
-    cardAnime.innerHTML = `
-    <div>
-    <img src= '${dato.poster}' id="image-poster"></img>
-    <p id="title"> ${dato.title}
-    <a href="title.html"> ${dato.title}</a>
-    </p>
-    <p id="release"> ${"(" + dato.release_date + ")"} </p>
-    <br>
-    </div>`
-    containerAnimes.appendChild(cardAnime);
-    
-}
+import { mostrarPelicula} from './data.js';
 
 
 
 
 
 
-
-
-
-
-console.log(data.films.map(director => director.director));
-
-
-
-document.addEventListener("click", function(){
-    document.getElementById("root").innerHTML = JSON.stringify(dire);
-  });
-
-
-const directores = document.getElementById("direct1");
-const dire= data.films.filter(item => 
-   item.director.includes(directores.value));
-
-console.log(dire);
 
 console.log(data.films.filter(item => item.director.includes(directores.value)));
+
+
 
 
 //console.log(example, data);
