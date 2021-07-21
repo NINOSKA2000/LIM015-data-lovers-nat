0f66dbad99f2678f9963bb15db6cf8e8fe8fbaac
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2021-07-20 23:05:31 -0500

    mostrador de funciones - coomit iniicnado

--- src/main.js
+++ src/main.js
@@ -8,3 +8,50 @@ import { mostrarPelicula} from './data.js';
 
 // imput:que hara la busqueda , selector que hara la busqueda del texto.
 //searchFilters(".searfilms",".container-card-anime");
+
+
+
+
+
+
+
+
+//dinamico boton desplegado
+addEventListener('DOMContentLoaded', () => {
+    const botonMenu = document.querySelector(".btn-menu");
+    if(botonMenu) {
+        botonMenu.addEventListener("click", () => {
+            const navMenu = document.querySelector('.nav-menu')
+            navMenu.classList.toggle('show')
+        })
+    }
+})
+
+
+
+
+
+
+//mostrador de detalles de tabs 
+
+const [tabs, tabsPanels] = [
+    
+	Array.from(document.querySelectorAll(".tabs li")),
+	Array.from(document.querySelectorAll(".tabs-panel"))
+];
+
+tabs.forEach((tab) => {
+	tab.addEventListener("click", () => {
+		const target = document.querySelector(`#${tab.dataset.target}`);
+		removeActiveClass([tabs, tabsPanels]);
+		tab.classList.add("active");
+		target.classList.add("active");
+	});
+});
+
+const removeActiveClass = (el) => {
+	el.forEach((item) => {
+		item.find((e) => e.classList.contains("active")).classList.remove("active");
+	});
+};
+
