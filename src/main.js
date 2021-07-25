73f594ede16084819e24dcf46efe3b37ae993faf
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2021-07-24 23:32:08 -0500

    nnanvn

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const boton = document.querySelector("#btn");
 const mensaje = document.querySelector("#mensaje-error");
 const selectedSpecie = document.getElementById("filter-by-species");
 const alphabetSelect = document.getElementById("order-by-alphabet");
+const modaldialog = document.querySelector(".modalDialog");
 
 
 
@@ -276,19 +277,6 @@ function vehiclesShow (contenedor, datatotal) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function mostrarPersonajes() {
     charactersHome.style.display="block";
     limpieza();
@@ -368,3 +356,53 @@ const removeActiveClass = (el) => {
 		item.find((e) => e.classList.contains("active")).classList.remove("active");
 	});
 };
+
+
+
+// graficos estadisticos 
+
+
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+chart();
+
+
+
+
+
+// modal de detalles mostrador 
+
